bf67c635a75b7ad4a63a92960f297daf91026584
snyssen <Simon.Nyssen@outlook.com>
2020-11-15 21:23:39 +0100

    Change center block so it takes full width on mobile

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NbIconConfig, NbMenuItem } from '@nebular/theme';
+import { Component, OnInit } from '@angular/core';
+import { NbIconConfig, NbMenuItem, NbThemeService } from '@nebular/theme';
 import { DEFAULT_NAV_ITEMS } from './DefaultNavigationItems';
 
 @Component({
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit {
 
+  isMobile = false;
   navigationItems: {title: string, icon?: string | NbIconConfig, items: NbMenuItem[]}[] = DEFAULT_NAV_ITEMS;
-  constructor() { }
+
+  constructor(private themeService: NbThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.onMediaQueryChange().subscribe(([previous, current]) => {
+      this.isMobile = ['xs', 'is', 'sm', 'md'].includes(current.name);
+    });
+  }
 
 }
